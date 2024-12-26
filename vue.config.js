30e4ba437729446ed7be3896eea41d4ce80366c5
baduyifei <baduyifei@qq.com>
2024-12-26 16:32:32 +0800

    Update vue.config.js: Add devServer configuration to auto-open browser and disable linting on save

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
+/** @format */
+
+const {defineConfig} = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+	transpileDependencies: true,
+	devServer: {
+		open: true, // 启动服务的时候,自动打开浏览器
+	},
+	lintOnSave: false, // 代码重新编译的时候,不需要 eslint 代码格式校验
+});
